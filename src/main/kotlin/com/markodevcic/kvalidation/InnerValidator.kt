c995defbbf9014ce16af4d6b8520a9f68951f840
Marko Devcic <madevcic@gmail.com>
2016-11-06 12:17:57 +0100

    method rename

--- src/main/kotlin/com/markodevcic/kvalidation/InnerValidator.kt
+++ src/main/kotlin/com/markodevcic/kvalidation/InnerValidator.kt
@@ -2,20 +2,18 @@ package com.markodevcic.kvalidation
 
 /**
  * Validator class that can be used inside of validated object
- * @sample<
- *      class Person(private val name: String, private val age: Int){
- *          val validator = InnerValidator(this) rulesInit {
- *              forProperty { p -> p.name } rules {
- *                  equal("John")
+ * @sample class Person(private val name: String, private val age: Int) {
+ *              val validator = InnerValidator(this) setRules {
+ *                  forProperty { p -> p.name } rules {
+ *                      equal("John")
+ *                  }
  *              }
  *          }
- *      }
- * >
  */
 class InnerValidator<T>(instance: T) : ValidatorBase<T>(instance) where T : Any {
 }
 
-infix fun <T> InnerValidator<T>.rulesInit(initializer: InnerValidator<T>.() -> Unit) : InnerValidator<T> where T : Any {
+infix fun <T> InnerValidator<T>.setRules(initializer: InnerValidator<T>.() -> Unit) : InnerValidator<T> where T : Any {
     initializer(this)
     return this
 }
